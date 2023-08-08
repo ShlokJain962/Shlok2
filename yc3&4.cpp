cc074db021162aa010a503e9692287514eb24bf1
ShlokJain962 <131585496+ShlokJain962@users.noreply.github.com>
2023-08-08 13:04:31 +0530

    Add files via upload

---
+++ yc3&4.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+// bhai lote to kaha tak pohoch gay
+// mehenat garuri hai 
+// fight on 
+int main(){
+    int x = 66, y = 34;
+    cout<<"hello,\n here x is equal to "<< x<<" & y is "<<y;
+    return 0;
+}
